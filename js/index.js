d7c933cbd596ac4e02577ec592ea3855d39ae04e
vivek9985 <vivekanando363@gmail.com>
2023-11-16 21:18:58 +0600

    modified some files

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
- //typing text animation script
+//typing text animation script
 var typed = new Typed(".typing_text", {
-    strings: ["Web Designer.", "Web Developer.", "WordPress Expert.", "Freelancer."],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-    cursorChar: '|️',
-    });  
+  strings: [
+    "Web Designer.",
+    "Web Developer.",
+    "WordPress Expert.",
+    "Freelancer.",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+  cursorChar: "|️",
+});
 var cursorOutline = document.querySelector(".cursor-outline");
 var cursorPoint = document.querySelector(".cursor-point");
 document.addEventListener("mousemove", (e) => {
-    cursorOutline.style.cssText = cursorPoint.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
-});    
+  cursorOutline.style.cssText = cursorPoint.style.cssText =
+    "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+});
